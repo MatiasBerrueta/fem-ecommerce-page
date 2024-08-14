7b23c477f3cb890f9cbad215f064e38068ce85e6
MatiasBerrueta <matias.berrueta1@gmail.com>
2024-08-14 18:22:21 -0300

    Ligthbox slider added

--- src/imageSlider.ts
+++ src/imageSlider.ts
@@ -4,16 +4,12 @@ const fullImage = document.querySelector('[data-open-ligthbox]') as HTMLImageEle
 const nextImageButton = document.querySelector('[data-next-image]')
 const previousImageButton = document.querySelector('[data-previous-image]')
 
-const nextImageButtonLigthbox = document.querySelector('[data-next-image-ligthbox]')
-const previousImageButtonLigthbox = document.querySelector('[data-previous-image-ligthbox]')
-
-const PRODUCT_IMAGES_QUANTITY = 4
+const PRODUCT_IMAGES_QUANTITY: number = 4
 let currentImage: number = 0
 thumbailImages[0].checked = true
 
 function autoSlide() {
-  currentImage === PRODUCT_IMAGES_QUANTITY - 1 ? currentImage = 0 : currentImage++
-  console.log(currentImage)
+  currentImage = (currentImage + 1) % PRODUCT_IMAGES_QUANTITY
   thumbailImages[currentImage].checked = true
   fullImage.style.marginLeft = `${-100 * (currentImage)}%`
 }
@@ -43,4 +39,44 @@ nextImageButton?.addEventListener('click', () => {
 previousImageButton?.addEventListener('click', () => {
   currentImage === 0 ?  currentImage = PRODUCT_IMAGES_QUANTITY - 1 : currentImage--
   swapImages()
+})
+
+const ligthBox = document.querySelector('[data-ligthbox]')
+const fullImageLigthbox = document.querySelector('[data-full-image-ligthbox]') as HTMLImageElement
+const closeLigthboxButton = document.querySelector('[data-close-ligthbox]')
+const thumbailImagesLigthbox: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="thumbail-product-images-ligthbox"]')
+
+const nextImageButtonLigthbox = document.querySelector('[data-next-image-ligthbox]')
+const previousImageButtonLigthbox = document.querySelector('[data-previous-image-ligthbox]')
+
+let currentLigthboxImage: number = 0
+thumbailImagesLigthbox[0].checked = true
+
+fullImage?.addEventListener('click', () => {
+	if(window.screen.width <= 700) return
+	ligthBox?.classList.remove('hide')
+})
+
+closeLigthboxButton?.addEventListener('click', () => {
+  ligthBox?.classList.add('hide')
+})
+
+previousImageButtonLigthbox?.addEventListener('click', () => {
+	currentLigthboxImage = (currentLigthboxImage - 1) % PRODUCT_IMAGES_QUANTITY
+	thumbailImagesLigthbox[currentLigthboxImage].checked = true
+	fullImageLigthbox.style.marginLeft = `${-100 * (currentLigthboxImage)}%`
+})
+
+nextImageButtonLigthbox?.addEventListener('click', () => {
+	currentLigthboxImage = (currentLigthboxImage + 1) % PRODUCT_IMAGES_QUANTITY
+	thumbailImagesLigthbox[currentLigthboxImage].checked = true
+	fullImageLigthbox.style.marginLeft = `${-100 * (currentLigthboxImage)}%`
+})
+
+thumbailImagesLigthbox.forEach((thumbailImage) => {
+  thumbailImage.addEventListener('click', (event) => {
+    const selectedElement = event.target as HTMLInputElement
+    currentLigthboxImage = parseInt(selectedElement.value) -1
+    fullImageLigthbox.style.marginLeft = `${-100 * (currentLigthboxImage)}%`
+  })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-const fullImageLigthbox = document.querySelector('[data-full-image-ligthbox]') as HTMLImageElement
-const closeLigthbox = document.querySelector('[data-close-ligthbox]')
-const ligthBox = document.querySelector('[data-ligthbox]')
-const thumbailImagesLigthbox: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="thumbail-product-images-ligthbox"]')
-
 const totalPrice = document.querySelector('[data-total-price]')
 const quantityProductCartCounter = document.querySelector('[data-product-quantity-cart]')
 const smallQuantityProductCartCounter = document.querySelector('[data-small-product-quantity-cart]')
@@ -10,17 +5,6 @@ const smallQuantityProductCartCounter = document.querySelector('[data-small-prod
 const mobileMenu = document.querySelector('[data-mobile-menu]')
 const mobileMenuOpen = document.querySelector('[data-mobile-menu-open]')
 const mobileMenuClose = document.querySelector('[data-mobile-menu-close]')
-
-let currentLigthboxImage = 1
-
-// fullImage?.addEventListener('click', () => {
-// 	if(window.screen.width <= 700) return
-// 	ligthBox?.classList.add('open')
-// })
-
-closeLigthbox?.addEventListener('click', () => {
-	ligthBox?.classList.remove('open')
-})
 
 mobileMenuOpen?.addEventListener('click', () => {
 	mobileMenu?.classList.remove('hide')
@@ -36,20 +20,6 @@ thumbailImagesLigthbox.forEach(thumbailImage => {
 		currentLigthboxImage = parseInt(selectedImage.value)
 		if(fullImageLigthbox) fullImageLigthbox.style.marginLeft = `${-100 * (currentLigthboxImage - 1)}%`
 	})
-})
-
-previousImageButtonLigthbox?.addEventListener('click', () => {
-	currentLigthboxImage--
-	if(currentLigthboxImage == 0) currentLigthboxImage = 4
-	thumbailImagesLigthbox[currentLigthboxImage - 1].checked = true
-	fullImageLigthbox.style.marginLeft = `${-100 * (currentLigthboxImage - 1)}%`
-})
-
-nextImageButtonLigthbox?.addEventListener('click', () => {
-	currentLigthboxImage++
-	if(currentLigthboxImage == 5) currentLigthboxImage = 1
-	thumbailImagesLigthbox[currentLigthboxImage - 1].checked = true
-	fullImageLigthbox.style.marginLeft = `${-100 * (currentLigthboxImage - 1)}%`
 })
 
 // previousImageButton?.addEventListener('click', () => {
